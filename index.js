35e7a6b46d2c831a3555077d57ceb69a152898d2
Isabel Lecourtois <rocleco@gmail.com>
2022-06-22 18:09:21 -0500

    Tercer commit, agregué un condicional

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Cotización de eventos
 
+// Constructor de Objetos para crear clientes
+
 let array = [];
 
 let nombre = prompt ("¿Cuál es tu nombre?")
@@ -20,25 +22,43 @@ const cliente1 = new Cliente (nombre, apellido, evento, hora, invitados);
 array.push(cliente1);
 console.log(array);
 
+// Switch case para hacer cotización
+
 let precioT 
 let precioI
 
 switch (hora){
-    case desayuno:
+    case "desayuno":
         precioI=100;
         precioT= precioI*invitados;
-        console.log("El precio de tu evento sería" + precioT);
+        console.log("El precio de tu evento sería " + precioT);
         break;
 
-    case comida:
+    case "comida":
         precioI=200;
-        console.log("El precio de tu evento sería" + precioT);
+        precioT= precioI*invitados;
+        console.log("El precio de tu evento sería " + precioT);
         break;
 
-    case cena:
-        precioT=150;
-        console.log("El precio de tu evento sería" + precioT);
-        break;        
+    case "cena":
+        precioI=150;
+        precioT= precioI*invitados;
+        console.log("El precio de tu evento sería " + precioT);
+        break;     
+        
+    default:
+        console.log("Escríbenos a nuestro teléfono de contacto para darte más información sobre nuestros servicios");
+        break;
+
+} 
+
+// Condicionales para saber si quiere la reserva o no
+
+let reserva= prompt ("¿Quieres continuar con tu reservación?");
+if (reserva == "si"){
+    prompt("¿Cuándo deseas que sea tu evento?");
+} else {
+    alert("vuelve pronto");
 }
 
 
