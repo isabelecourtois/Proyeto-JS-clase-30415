3c3c5bcadf735c84b0e479edcb1c42d6aabf2803
Isabel Lecourtois <rocleco@gmail.com>
2022-07-03 15:27:27 -0500

    Quinto commit, agregué un find como función de órden superior

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // Cotización de eventos
 
+// Catálogo de servicios
+
+const servicios = [
+    {servicio: "desayuno continental", precio: 150},
+    {servicio: "desayuno buffet", precio: 200},
+    {servicio: "comida corrida", precio: 200},
+    {servicio: "comida buffet", precio: 300},
+    {servicio: "cena ligera", precio: 100},
+    {servicio: "cena buffet", precio: 200},
+];
+
 // Constructor de Objetos para crear clientes
 
 let cuestionario = [];
@@ -7,63 +18,46 @@ let cuestionario = [];
 let nombre = prompt ("¿Cuál es tu nombre?").toLowerCase()
 let apellido = prompt ("¿Cuál es tu apellido?").toLowerCase()
 let evento = prompt ("¿Qué tipo de evento quieres cotizar?").toLowerCase()
-let hora = prompt ("Desayuno, comida o cena").toLowerCase()
+let fecha = prompt ("¿Cuándo deseas que sea tu evento? (dd/mm/aa").toLowerCase()
+let servicio = prompt ("Qué servicio quieres para tu evento").toLowerCase()
 let invitados = prompt ("¿Cuántos invitados habrá?").toLowerCase()
 
-    function Cliente (nombre, apellido, evento, hora, invitados){
+    function Cliente (nombre, apellido, evento, fecha, servicio, invitados){
         this.nombre= nombre;
         this.apellido= apellido;
         this.evento= evento;
-        this.hora= hora;
+        this.fecha= fecha;
+        this.servicio= servicio;
         this.invitados= invitados;
     };
 
-const cliente1 = new Cliente (nombre, apellido, evento, hora, invitados);
+const cliente1 = new Cliente (nombre, apellido, evento, fecha, servicio, invitados);
 cuestionario.push(cliente1);
-console.log(cuestionario);
+console.log(cuestionario); 
 
+/* while (nombre, apellido, evento, fecha, servicio, invitados =! "");
+{
+ */
 
-// Switch case para hacer cotización
+// Precio del servicio
 
-let precioT 
-let precioI
+let buscaServicio = servicios.find (elemento => elemento.servicio === servicio);
+let precioTotal = buscaServicio.precio * invitados;
+alert ("El precio de tu evento sería " + precioTotal);
 
-switch (hora){
-    case "desayuno":
-        precioI=100;
-        precioT= precioI*invitados;
-        console.log("El precio de tu evento sería " + precioT);
-        break;
-
-    case "comida":
-        precioI=200;
-        precioT= precioI*invitados;
-        console.log("El precio de tu evento sería " + precioT);
-        break;
-
-    case "cena":
-        precioI=150;
-        precioT= precioI*invitados;
-        console.log("El precio de tu evento sería " + precioT);
-        break;     
-        
-    default:
-        console.log("Escríbenos a nuestro teléfono de contacto para darte más información sobre nuestros servicios");
-        break;
-
-} 
 
 // Condicionales para saber si quiere la reserva o no
 
 let reserva= prompt ("¿Quieres continuar con tu reservación?").toLowerCase();
 if (reserva == "si"){
-    prompt("¿Cuándo deseas que sea tu evento? (dd/mm/aa");
     console.log ("Tu reserva está hecha");
+
 
 } else {
     alert("vuelve pronto");
 }
+/* }
 
-
+alert ("Tus datos están incompletos, por favor rellena el formulario de nuevo"); */
 
 
