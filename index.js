f3f3e0301e2b1cbf9dc757225b950503f61d8eb1
Isabel Lecourtois <rocleco@gmail.com>
2022-07-24 19:59:05 -0500

    Onceavo commit, re-entrega de la segunda pre-entrega

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-// Cotización de eventos
- 
-// Constructor de Objetos para crear clientes  
+//Declaro mis variables
+let botonCotizar = document.getElementById("btnCotizar");
+let nombre = document.getElementById("nombre");
+let evento = document.getElementById("evento");
+let fecha = document.getElementById("fecha");
+let servicio = document.getElementById("servicio");
+let invitados = document.getElementById("invitados");
 
-    let cuestionario = [];
-
-    let nombre = document.getElementById("nombre");
-    let evento = document.getElementById("evento");
-    let fecha = document.getElementById("fecha");
-    let servicio = document.getElementById("servicio");
-    let invitados = document.getElementById("invitados");
-
-    function Cliente (nombre, evento, fecha, servicio, invitados){
-        this.nombre= nombre;
-        this.evento= evento;
-        this.fecha= fecha;
-        this.servicio= servicio;
-        this.invitados= invitados;
-
-    };
-
-    let formulario = document.getElementById("form");
-    formulario.addEventListener ("submit", validarFormulario);
-    
-    function validarFormulario (e){
-        e.preventDefault();
-
-
-    const cliente1 = new Cliente (nombre.value, evento.value, fecha.value, servicio.value, invitados.value);
-    cuestionario.push(cliente1);
-    console.log(cuestionario); 
-
-   
-
-// Catálogo de servicios
-
+//Catálogo de servicios
 
 const servicios = [
     {servicio: "Desayuno Continental", precio: 150},
@@ -44,50 +17,76 @@ const servicios = [
     {servicio: "Cena Buffet", precio: 200},
 ];
 
-// Precio del servicio
+// Constructor de Objetos para crear clientes  
 
-let buscaServicio = servicios.find (elemento => elemento.servicio === servicio.value);
-let precioTotal = buscaServicio.precio * invitados.value;
-console.log(precioTotal);
+const hacerCuestionario = () => {
+    let cuestionario = []; 
+    
+    function Cliente (nombre, evento, fecha, servicio, invitados){
+        this.nombre= nombre;
+        this.evento= evento;
+        this.fecha= fecha;
+        this.servicio= servicio;
+        this.invitados= invitados;
+    
+    };
+    
+    const cliente1 = new Cliente (nombre.value, evento.value, fecha.value, servicio.value, invitados.value);
+    cuestionario.push(cliente1);
+    console.log(cliente1); 
 
+    localStorage.setItem("cuestionario",JSON.stringify (cuestionario));    
+    };
 
-// Cotización en el HTML
+// Hacer Cotización
+
 const hacerCotizacion = () => {
+
+// Precio del servicio
+    let buscaServicio = servicios.find (elemento => elemento.servicio === servicio.value);
+    let precioTotal = buscaServicio.precio * invitados.value;
+    console.log(precioTotal);
+
+//Cotización en HTML
+
     cotizacion.innerHTML = `<div class="cotizacionFinal">
                                 <h2>HOLA: ${nombre.value}</h2>
-                                <p>Tu cotización para tu ${evento.value} sería $ ${precioTotal} </p>
+                                <p>Tu cotización para tu ${evento.value} es de $${precioTotal}  </p>
 
                             </div>`
 };
+const validarCampos = () => {
+    let validacion = (nombre.value == "" || evento.value == "" || fecha.value == "" || servicio.value == "" || invitados.value == "") ? false : true;
+    return validacion
+};
 
 
-let boton = document.getElementById("btnCotizar")
-boton.onclick = () => hacerCotizacion()
-
-
-// Ver Cotizaciones Pasadas
-
-// Guardas en el localStorage
-
-localStorage.setItem("cuestionario",JSON.stringify (cuestionario));    
-let cotisPasadas =JSON.parse(localStorage.getItem("cuestionario")) || [];
-console.log(cotisPasadas);
-
+botonCotizar.onclick = (e) =>{
+    e.preventDefault();
+   hacerCuestionario ();
+   validarCampos() ? hacerCotizacion() : swal("Debe completar todos los campos.");
+};
 
 // Cotizaciones pasadas en el HTML
+
 const verCotizacion = () => {
-    cotisPasadas.innerHTML = `<div class="cotizacionFinal">
-                                <h2>HOLA: ${nombre.value}</h2>
-                                <p>Tu cotización para tu ${evento.value} sería $ ${precioTotal} </p>
+    let cotisPasadas =JSON.parse(localStorage.getItem("cuestionario")) || [];
+    console.log(cotisPasadas);  
+    for (const producto of cotisPasadas){
+        console.log(producto.nombre);
+    
+
+    cotizacionesPasadas.innerHTML = `<div class="cotizacionFinal">
+                                <h2>¡Hola!</h2>
+                                <p>Tu última cotización es un ${producto.evento} para ${producto.invitados} personas el día ${producto.fecha}</p>
 
                             </div>`
+}
 };
 
 
-//No agarra el botón, no sé por qué
 let boton2 = document.getElementById("btnCotisPasadas")
 boton2.onclick = (e) => {
     e.preventDefault();
     verCotizacion ()
 };
-}
